5f8a0f9a17b577f1a54fe39f62cadc00d39d1a8f
Manuele Longo <mlongo@e4r8p2.42roma.it>
2023-12-14 16:55:08 +0100

    fix Python04 ex03

--- Piscine/Exercises/Python04/ex03/dump_word_dict.py
+++ Piscine/Exercises/Python04/ex03/dump_word_dict.py
@@ -2,10 +2,9 @@ def main():
 	import pickle
 	with open('words.txt', 'r') as file:
 		lines = file.readlines()
-		print(lines)
 		dct = dict()
 		for line in lines:
-			dct[len(line) - 1] = dct.get(len(line) - 1, 0) + 1
+			dct[len(line.replace("\n", ""))] = dct.get(len(line.replace("\n", "")), 0) + 1
 		with open('word_count.pickle', 'wb') as file2:
 			pickle.dump(dct, file2)
 
